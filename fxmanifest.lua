7a50e9b43515624dcbf2d512ff5d7c5df4f045f3
vanishdevs <22zVanish@gmail.com>
2024-07-30 18:04:49 -0400

    add(server): Add a version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,14 @@
 fx_version 'adamant'
 game 'gta5'
 lua54 'yes'
+version '1.0.0'
 
 author 'vanishdev'
 description 'A simple looking hud, UI was inspired from another resource, many features included.'
 
 shared_script '@ox_lib/init.lua'
 client_scripts { 'client/*.lua' }
+server_scripts { 'server/*.lua' }
 
 ui_page 'web/index.html'
 files { 'web/**', 'data/*.lua' }
---
+++ server/main.lua
@@ -0,0 +1,1 @@
+lib.versionCheck('vanishdevs/vanish_hud')
